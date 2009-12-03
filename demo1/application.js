0b548e30e702b9e0f29ca0649af596cc82156ec7
Andre Goncalves <andre@questionform.com>
2009-12-03 18:19:54 +0000

    more crazy experiences

--- demo1/application.js
+++ demo1/application.js
@@ -5,23 +5,69 @@ $(document).ready(function() {
 		{
 			startRecording();
 			$(this).removeClass('stopped');
+			$(this).text('stop recording');			
 		}
 		else
 		{
 			// TODO: move this to a pubsub
 			stopRecording();
 			$(this).addClass('stopped');
+			$(this).text('start recording');
 		}
-		
-		
 	});
 	
-	
-	$(document).mousemove(function(e) {
-		console.info(e);
+	$("#play").click(function() {
+		playRecording();
 	});
 });
 
 var eventStorage = [];
 
+
+function storeEvent(e) {
+	eventStorage.push(e);
+}
+
+
+function startRecording() {
+	$(document).bind('mousemove', storeEvent);
+}
+
+function stopRecording() {
+	$(document).unbind('mousemove', storeEvent);
+}
+
+function playRecording() {
+	$.each(eventStorage, function(index, el) {
+		
+		//console.info("X,Y: " + el.pageX + "," + el.pageY);
+		window["run_"+el.type](el);
+	});
+}
+
+
+function run_mousemove(e) {
+
+	$("#mouse").css('top', e.pageY).css('left', e.pageX);
+	console.info("tick");
+	sleep(2);
+}
+
+
+
+function sleep(naptime)
+{
+	naptime = naptime * 1000;
+  var sleeping = true;
+  var now = new Date();
+  var alarm;
+	var startingMSeconds = now.getTime();
+	//alert("starting nap at timestamp: " + startingMSeconds + "\nWill sleep for: " + naptime + " ms");
+	while(sleeping){
+		alarm = new Date();
+		alarmMSeconds = alarm.getTime();
+		if(alarmMSeconds - startingMSeconds > naptime){ sleeping = false; }
+	}        
+	//alert("Wakeup!");
+}
 
